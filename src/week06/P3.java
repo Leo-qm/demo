0215a6151455cc7d37b023f54765f88e8942b272
lianyayu <lianyayu@knowin.com>
2021-06-07 15:11:11 +0800

    字符串内容

---
+++ src/week06/P3.java
@@ -0,0 +1,12 @@
+package week06;
+
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        String s;
+        s = in.next();
+        System.out.println(s.equals("bye"));
+    }
+}
